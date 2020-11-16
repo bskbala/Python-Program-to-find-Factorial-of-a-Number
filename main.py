68cb80f5a5e7d19e3fecd95e97a6b8483885f753
Bandiwala <bskbala410@gmail.com>
2020-11-16 06:00:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Python Program to find Factorial of a Number #
+
+import math
+
+number = int(input("[Please Enter the Number:"))
+
+fact = math.factorial(number)
+print("the factorial of %d = %d"%(number,fact))
